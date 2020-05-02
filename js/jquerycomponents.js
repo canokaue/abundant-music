529f0658ebef3a0e1c3c1474d8e038044206e5c7
Paul Pacheco <00557659>
2020-05-02 13:22:42 -0500

    use es6 classes

--- js/jquerycomponents.js
+++ js/jquerycomponents.js
@@ -1,220 +1,191 @@
 
 
-function JQueryComponent() {
-    this.$component = null;
-    this.cssClassName = "component"; // Main class
-    this.otherCssClasses = [];
-    this.id = "";
-    this.tagName = "div";
-    this.children = [];
-    this._constructorName = "JQueryComponent";
-}
-
-JQueryComponent.prototype.counters = {};
-
-JQueryComponent.prototype.setUniqueId = function() {
-    let counter = JQueryComponent.prototype.counters[this.cssClassName];
-    if (!counter) {
-        counter = 1;
-    } else {
-        counter++;
+class JQueryComponent {
+    constructor() {
+        this.$component = null;
+        this.cssClassName = "component"; // Main class
+        this.otherCssClasses = [];
+        this.id = "";
+        this.tagName = "div";
+        this.children = [];
+        this._constructorName = "JQueryComponent";
     }
-    JQueryComponent.prototype.counters[this.cssClassName] = counter;
-    this.id = this.cssClassName + "-" + counter;
-};
-
-
-JQueryComponent.prototype.hide = function() {
-    this.$component.hide();
-};
-
-JQueryComponent.prototype.show = function() {
-    this.$component.show();
-};
-
-JQueryComponent.prototype.addChild = function(c) {
-    this.children.push(c);
-};
-
-
-JQueryComponent.prototype.addStartHtmlString = function(resultArr) {
-    resultArr.push("<" + this.tagName + " " +
-        "class=\"" + this.cssClassName + (this.otherCssClasses.length > 0 ? " " + this.otherCssClasses.join(" ") : "") + "\" " +
-        (this.id ? "id=\"" + this.id + "\" " : "") +
-        this.getTagAttributeString() +
-        " >");
-};
-
-JQueryComponent.prototype.getTagAttributeString = function() {
-    let result = "";
-    const obj = {};
-    this.getTagAttributes(obj);
-    for (const atr in obj) {
-        result += " " + atr + "=\"" + obj[atr] + " ";
-    }
-    return result;
-};
-
-JQueryComponent.prototype.getTagAttributes = function(obj) {
-};
-
-JQueryComponent.prototype.addEndHtmlString = function(resultArr) {
-    resultArr.push("</" + this.tagName + ">");
-};
-
-
-JQueryComponent.prototype.spawn = function(parent) {
-    const strings = [];
-    this.createJQueryStrings(strings);
-    const $item = $(strings.join(''));
-    let $parentComponent = parent.$component;
-    if (!$parentComponent) {
-        $parentComponent = parent;
-    }
-    $parentComponent.append($item);
-    this.jQueryCreated($parentComponent);
-};
-
-JQueryComponent.prototype.jQueryCreated = function($localRoot) {
-    const selector = "." + this.cssClassName;
-    if ($localRoot.is(selector)) {
-        this.$component = $localRoot;
-    } else {
-        this.$component = $localRoot.find("." + this.cssClassName);
-    }
-    if (this.id) {
-        this.$component = this.$component.filter("#" + this.id);
-    }
-    for (let i=0; i<this.children.length; i++) {
-        this.children[i].jQueryCreated($localRoot);
-    }
-};
-
-JQueryComponent.prototype.createJQueryStrings = function(resultArr) {
-    this.addStartHtmlString(resultArr);
-    this.getHtmlContentBeforeChildren(resultArr);
-    for (let i=0; i<this.children.length; i++) {
-        this.getHtmlContentBeforeChild(resultArr, i);
-        this.children[i].createJQueryStrings(resultArr);
-        this.getHtmlContentAfterChild(resultArr, i);
-    }
-    this.getHtmlContentAfterChildren(resultArr);
-    this.addEndHtmlString(resultArr);
-};
-
-JQueryComponent.prototype.getHtmlContentBeforeChildren = function(resultArr) {
-};
-
-JQueryComponent.prototype.getHtmlContentAfterChildren = function(resultArr) {
-};
-
-JQueryComponent.prototype.getHtmlContentBeforeChild = function(resultArr, childIndex) {
-};
-JQueryComponent.prototype.getHtmlContentAfterChild = function(resultArr, childIndex) {
-};
-
-
-JQueryComponent.prototype.enable = function() {
-};
-
-JQueryComponent.prototype.disable = function() {
-};
-
-function JQueryButton(options) {
-    JQueryComponent.call(this, options);
-    this.tagName = "button";
-    this.text = getValueOrDefault(options, "text", "");
-    this.enableText = getValueOrDefault(options, "enableText", true);
-    this.primaryIcon = getValueOrDefault(options, "primaryIcon", "");
-    this.secondaryIcon = getValueOrDefault(options, "secondaryIcon", "");
-    this.cssClassName = "jquery-button";
-    this.setUniqueId();
-    this.clickListeners = [];
-    this._constructorName = "JQueryButton";
-}
-
-JQueryButton.prototype = new JQueryComponent();
-
-
-JQueryButton.prototype.enable = function() {
-    this.$component.button("enable");
-};
-
-JQueryButton.prototype.disable = function() {
-    this.$component.button("disable");
-};
-
-JQueryButton.prototype.jQueryCreated = function($localRoot) {
-    JQueryComponent.prototype.jQueryCreated.call(this, $localRoot);
-    const buttonOptions = {};
-    buttonOptions.text = this.enableText;
-    if (this.primaryIcon || this.secondaryIcon) {
-        buttonOptions.icons = {};
-        if (this.primaryIcon) {
-            buttonOptions.icons["primary"] = this.primaryIcon;
+    setUniqueId() {
+        let counter = JQueryComponent.counters[this.cssClassName];
+        if (!counter) {
+            counter = 1;
         }
-        if (this.secondaryIcon) {
-            buttonOptions.icons["secondary"] = this.secondaryIcon;
+        else {
+            counter++;
+        }
+        JQueryComponent.counters[this.cssClassName] = counter;
+        this.id = this.cssClassName + "-" + counter;
+    }
+    hide() {
+        this.$component.hide();
+    }
+    show() {
+        this.$component.show();
+    }
+    addChild(c) {
+        this.children.push(c);
+    }
+    addStartHtmlString(resultArr) {
+        resultArr.push("<" + this.tagName + " " +
+            "class=\"" + this.cssClassName + (this.otherCssClasses.length > 0 ? " " + this.otherCssClasses.join(" ") : "") + "\" " +
+            (this.id ? "id=\"" + this.id + "\" " : "") +
+            this.getTagAttributeString() +
+            " >");
+    }
+    getTagAttributeString() {
+        let result = "";
+        const obj = {};
+        this.getTagAttributes(obj);
+        for (const atr in obj) {
+            result += " " + atr + "=\"" + obj[atr] + " ";
+        }
+        return result;
+    }
+    getTagAttributes(obj) {
+    }
+    addEndHtmlString(resultArr) {
+        resultArr.push("</" + this.tagName + ">");
+    }
+    spawn(parent) {
+        const strings = [];
+        this.createJQueryStrings(strings);
+        const $item = $(strings.join(''));
+        let $parentComponent = parent.$component;
+        if (!$parentComponent) {
+            $parentComponent = parent;
+        }
+        $parentComponent.append($item);
+        this.jQueryCreated($parentComponent);
+    }
+    jQueryCreated($localRoot) {
+        const selector = "." + this.cssClassName;
+        if ($localRoot.is(selector)) {
+            this.$component = $localRoot;
+        }
+        else {
+            this.$component = $localRoot.find("." + this.cssClassName);
+        }
+        if (this.id) {
+            this.$component = this.$component.filter("#" + this.id);
+        }
+        for (let i = 0; i < this.children.length; i++) {
+            this.children[i].jQueryCreated($localRoot);
         }
     }
-    this.$component.button(buttonOptions);
-    this.$component.click(this, this.buttonClick);
+    createJQueryStrings(resultArr) {
+        this.addStartHtmlString(resultArr);
+        this.getHtmlContentBeforeChildren(resultArr);
+        for (let i = 0; i < this.children.length; i++) {
+            this.getHtmlContentBeforeChild(resultArr, i);
+            this.children[i].createJQueryStrings(resultArr);
+            this.getHtmlContentAfterChild(resultArr, i);
+        }
+        this.getHtmlContentAfterChildren(resultArr);
+        this.addEndHtmlString(resultArr);
+    }
+    getHtmlContentBeforeChildren(resultArr) {
+    }
+    getHtmlContentAfterChildren(resultArr) {
+    }
+    getHtmlContentBeforeChild(resultArr, childIndex) {
+    }
+    getHtmlContentAfterChild(resultArr, childIndex) {
+    }
+    enable() {
+    }
+    disable() {
+    }
 
-//    logit("button " + this.$component.size() + " id: " + this.id);
-};
-
-JQueryButton.prototype.buttonClick = function(event) {
-    const button = event.data;
-    $.each(button.clickListeners, function(key, value) {
-        //        logit("bc data " + value.data + "<br />");
-        value.clicked(value.data);
-    });
-//    $.each(event, function(key, value) {
-//        logit(" " + key + ":" + value + "<br />");
-//    });
-};
-
-JQueryButton.prototype.addClickListener = function(l) {
-    this.clickListeners.push(l);
-    return this;
-};
-
-JQueryButton.prototype.getHtmlContentBeforeChildren = function(resultArr) {
-    resultArr.push(this.text);
-};
-
-
-function JQueryPanel() {
-    JQueryComponent.call(this);
-    this._constructorName = "JQueryPanel";
+    static counters = {}
 }
 
-JQueryPanel.prototype = new JQueryComponent();
 
-
-function JQueryListItem() {
-    JQueryPanel.call(this);
-    this.cssClassName = "list-item";
-    this.otherCssClasses.push("ui-widget-content");
-    this.tagName = "li";
-    this._constructorName = "JQueryListItem";
+class JQueryButton extends JQueryComponent {
+    constructor(options) {
+        super()
+        this.tagName = "button";
+        this.text = getValueOrDefault(options, "text", "");
+        this.enableText = getValueOrDefault(options, "enableText", true);
+        this.primaryIcon = getValueOrDefault(options, "primaryIcon", "");
+        this.secondaryIcon = getValueOrDefault(options, "secondaryIcon", "");
+        this.cssClassName = "jquery-button";
+        this.setUniqueId();
+        this.clickListeners = [];
+        this._constructorName = "JQueryButton";
+    }
+    enable() {
+        this.$component.button("enable");
+    }
+    disable() {
+        this.$component.button("disable");
+    }
+    jQueryCreated($localRoot) {
+        JQueryComponent.prototype.jQueryCreated.call(this, $localRoot);
+        const buttonOptions = {};
+        buttonOptions.text = this.enableText;
+        if (this.primaryIcon || this.secondaryIcon) {
+            buttonOptions.icons = {};
+            if (this.primaryIcon) {
+                buttonOptions.icons["primary"] = this.primaryIcon;
+            }
+            if (this.secondaryIcon) {
+                buttonOptions.icons["secondary"] = this.secondaryIcon;
+            }
+        }
+        this.$component.button(buttonOptions);
+        this.$component.click(this, this.buttonClick);
+        //    logit("button " + this.$component.size() + " id: " + this.id);
+    }
+    buttonClick(event) {
+        const button = event.data;
+        $.each(button.clickListeners, function (key, value) {
+            //        logit("bc data " + value.data + "<br />");
+            value.clicked(value.data);
+        });
+        //    $.each(event, function(key, value) {
+        //        logit(" " + key + ":" + value + "<br />");
+        //    });
+    }
+    addClickListener(l) {
+        this.clickListeners.push(l);
+        return this;
+    }
+    getHtmlContentBeforeChildren(resultArr) {
+        resultArr.push(this.text);
+    }
 }
 
-JQueryListItem.prototype = new JQueryPanel();
-
-function JQueryVerticalListItemDragHandle() {
-    JQueryComponent.call(this);
-    this.iconClass = "ui-icon-carat-2-n-s";
-    this.otherCssClasses.push("vertical-list-item-drag-handle");
-    this._constructorName = "JQueryVerticalListItemDragHandle";
+class JQueryPanel extends JQueryComponent {
+    constructor() {
+        super()
+        this._constructorName = "JQueryPanel";
+    }
 }
 
-JQueryVerticalListItemDragHandle.prototype = new JQueryComponent();
+class JQueryListItem extends JQueryPanel{
+    constructor() {
+        super()
+        this.cssClassName = "list-item";
+        this.otherCssClasses.push("ui-widget-content");
+        this.tagName = "li";
+        this._constructorName = "JQueryListItem";
+    }
+}
 
-
-JQueryVerticalListItemDragHandle.prototype.getHtmlContentBeforeChildren = function(resultArr) {
-    resultArr.push("<span class='ui-icon " + this.iconClass + "'></span>");
-};
-
-
+class JQueryVerticalListItemDragHandle extends JQueryComponent {
+    constructor() {
+        super()
+        this.iconClass = "ui-icon-carat-2-n-s";
+        this.otherCssClasses.push("vertical-list-item-drag-handle");
+        this._constructorName = "JQueryVerticalListItemDragHandle";
+    }
+    getHtmlContentBeforeChildren(resultArr) {
+        resultArr.push("<span class='ui-icon " + this.iconClass + "'></span>");
+    }
+}
 
