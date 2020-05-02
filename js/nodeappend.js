7cdb9c94f2ab56cd9fdd0fb616512331aaa17193
Paul Pacheco <00557659>
2020-05-02 13:22:42 -0500

    Use es6 module

--- js/nodeappend.js
+++ js/nodeappend.js
@@ -1,33 +1,32 @@
 
-exports.objectToJson = objectToJson;
-exports.createTestModule = createTestModule;
-exports.arrayContains = arrayContains;
-exports.arrayDelete = arrayDelete;
-exports.objectToJson = objectToJson;
-exports.userToDirName = userToDirName;
-exports.validateValueWithSafeValue = validateValueWithSafeValue;
+export const objectToJson = objectToJson;
+export const createTestModule = createTestModule;
+export const arrayContains = arrayContains;
+export const arrayDelete = arrayDelete;
+export const userToDirName = userToDirName;
+export const validateValueWithSafeValue = validateValueWithSafeValue;
 
-exports.MersenneTwister = MersenneTwister;
-exports.StaticHarmonyGenerator = StaticHarmonyGenerator;
-exports.DynamicHarmonyGenerator = DynamicHarmonyGenerator;
-exports.ConstantHarmonicRythm = ConstantHarmonicRythm;
-exports.MidiProgram = MidiProgram;
-exports.MidiDrum = MidiDrum;
-exports.SimpleModuleGeneratorPhraseGroupType = SimpleModuleGeneratorPhraseGroupType;
-exports.DynamicHarmonyModulationTarget = DynamicHarmonyModulationTarget;
-exports.RenderData = RenderData;
-exports.RenderState = RenderState;
-exports.GenInfo = GenInfo;
+export const MersenneTwister = MersenneTwister;
+export const StaticHarmonyGenerator = StaticHarmonyGenerator;
+export const DynamicHarmonyGenerator = DynamicHarmonyGenerator;
+export const ConstantHarmonicRythm = ConstantHarmonicRythm;
+export const MidiProgram = MidiProgram;
+export const MidiDrum = MidiDrum;
+export const SimpleModuleGeneratorPhraseGroupType = SimpleModuleGeneratorPhraseGroupType;
+export const DynamicHarmonyModulationTarget = DynamicHarmonyModulationTarget;
+export const RenderData = RenderData;
+export const RenderState = RenderState;
+export const GenInfo = GenInfo;
 
-exports.moduleConstructTimer = moduleConstructTimer;
-exports.composeTimer = composeTimer;
-exports.harmonyTimer = harmonyTimer;
-exports.voiceLeadingTimer = voiceLeadingTimer;
-exports.voiceLeadingPrepareTimer = voiceLeadingPrepareTimer;
-exports.figurationTimer = figurationTimer;
-exports.perfTimer1 = perfTimer1;
-exports.perfTimer2 = perfTimer2;
-exports.perfTimer3 = perfTimer3;
+export const moduleConstructTimer = moduleConstructTimer;
+export const composeTimer = composeTimer;
+export const harmonyTimer = harmonyTimer;
+export const voiceLeadingTimer = voiceLeadingTimer;
+export const voiceLeadingPrepareTimer = voiceLeadingPrepareTimer;
+export const figurationTimer = figurationTimer;
+export const perfTimer1 = perfTimer1;
+export const perfTimer2 = perfTimer2;
+export const perfTimer3 = perfTimer3;
 
 function logit(str) {
     console.log(str);
