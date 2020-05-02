b5c06b86d6d2c9bb1c71f1de183e680c529835d4
Paul Pacheco <00557659>
2020-05-02 13:22:42 -0500

    use es6 classes

--- js/composer/structure.js
+++ js/composer/structure.js
@@ -1,38 +1,34 @@
 
-function Structure() {
-    this.id = "structure";
-    this.references = [];
-    this._constructorName = "Structure";
-}
-
-
-Structure.prototype.renderBatch = function(state, progressFunc) {
-
-    composeTimer.start();
-    for (let i=0; i<this.references.length; i++) {
-        const ref = this.references[i];
-        if (ref.active) {
+class Structure {
+    constructor() {
+        this.id = "structure";
+        this.references = [];
+        this._constructorName = "Structure";
+    }
+    renderBatch(state, progressFunc) {
+        composeTimer.start();
+        for (let i = 0; i < this.references.length; i++) {
+            const ref = this.references[i];
+            if (ref.active) {
+                ref.renderBatch(state);
+            }
+            if (progressFunc) {
+                let fraction = 1;
+                if (this.references.length > 1) {
+                    fraction = i / (this.references.length - 1);
+                }
+                progressFunc(fraction);
+            }
+        }
+        composeTimer.pause();
+    }
+    renderSection(state, index) {
+        const ref = this.references[index];
+        if (ref) {
             ref.renderBatch(state);
         }
-        if (progressFunc) {
-            let fraction = 1;
-            if (this.references.length > 1) {
-                fraction = i / (this.references.length - 1);
-            }
-            progressFunc(fraction);
+        else {
+            logit(this._constructorName + ": Could not find section with index " + index);
         }
     }
-    composeTimer.pause();
-};
-
-
-Structure.prototype.renderSection = function(state, index) {
-    const ref = this.references[index];
-    if (ref) {
-        ref.renderBatch(state);
-    } else {
-        logit(this._constructorName + ": Could not find section with index " + index);
-    }
-};
-
-
+}
