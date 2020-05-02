f69d76e8c1ed97765d2565223f8771842b3ebf14
Paul Pacheco <00557659>
2020-05-02 13:22:42 -0500

    Simplify loading more js files

--- js/loader.js
+++ js/loader.js
@@ -5,26 +5,3 @@ function updateLoaderProgress(progress) {
         $('#loader-progress').progressbar('option', 'value', progress);
     }
 }
-
-
-var useDevSources = true;
-var clientSources = ["css/style.css"];
-if (useDevSources) {
-    clientSources = "css/style.css js/midi.js js/fakebytearray.js js/tween.js js/classicalnoise.js js/jquerycomponents.js js/guiproperties.js js/guipropertiescomponent.js js/valuecomponents.js js/guiobjectlistcomponent.js js/uniqueidmanager.js js/propertyinfoprovider.js js/songsettingscomponents.js js/asyncoperation.js js/noterepr.js js/audioplayer.js js/sm2player.js js/webaudioplayer.js js/frustumcullingchunks.js js/composevisualizer.js js/composemain.js".split(" ");
-}
-
-Modernizr.load(
-    [
-        {
-            both: clientSources,
-            complete: function() {
-                updateLoaderProgress(50);
-
-                $(document).ready(function() {
-                    composeSetup1();
-                });
-            }
-        }
-    ]
-);
-
