c2b7be5376e1a802227e25c4d11fbc17a0e764aa
Paul Pacheco <paulpach@gmail.com>
2020-05-25 16:19:00 -0500

    use classes

--- js/async.js
+++ js/async.js
@@ -504,15 +504,19 @@
 
     async.iterator = function (tasks) {
         const makeCallback = function (index) {
-            const fn = function () {
-                if (tasks.length) {
-                    tasks[index].apply(null, arguments);
+            class fn {
+                constructor() {
+                    if (tasks.length) {
+                        tasks[index].apply(null, arguments);
+                    }
+                    return fn.next();
                 }
-                return fn.next();
-            };
-            fn.next = function () {
-                return (index < tasks.length - 1) ? makeCallback(index + 1): null;
-            };
+
+                static next() {
+                    return (index < tasks.length - 1) ? makeCallback(index + 1): null;
+                }
+            }
+
             return fn;
         };
         return makeCallback(0);
